ac8a267d37cf2110df026e4907950680ea88e662
sabaresh11 <sabaresh.1406029@srec.ac.in>
2017-08-14 17:49:55 +0530

    youtube videos embedded in front(default) page

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -5,7 +5,7 @@ import {ListComponent } from './list/list.component';
 import {DefaultDispComponent } from './default-disp/default-disp.component';
  
 export const router : Routes = [
-	// { path: '',redirectTo: 'default-disp',pathMatch: 'full'},
+	{ path: '',redirectTo: 'default',pathMatch: 'full'},
 	{ path: 'default', component:DefaultDispComponent },
 	{ path : 'list' , component:ListComponent}
 ];
